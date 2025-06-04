c0101a6285f962d9e644bd83bb348ce71b336568
sindhooraprabhu <sindhooranadsar3532@gmail.com>
2025-06-05 05:11:01 +0530

    Add create-post page, images, and style updates

---
+++ main.js
@@ -0,0 +1,1 @@
+document.getElementById("app").innerHTML = "<p>This is from main.js</p>";
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+// });
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  build: {
+    outDir: 'dist', // Vercel expects this folder
+    emptyOutDir: true,
+  },
+  base: './', // relative paths to load CSS/JS
 });
